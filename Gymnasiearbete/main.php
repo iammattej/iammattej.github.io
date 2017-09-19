089d8cde3c911ee610cf567d2674630c1f84d6da
iammattej <iammattej@gmail.com>
2017-09-19 11:19:44 +0200

    Update main.php

--- Gymnasiearbete/main.php
+++ Gymnasiearbete/main.php
@@ -1,17 +1,3 @@
-<?php
-    error_reporting (E_ALL ^ E_NOTICE^ E_DEPRECATED);
-    session_start();
-    $id = $_SESSION['id'];
-    $email = $_SESSION['email'];
- /*   if (!isset($_SESSION['email']))
-    {
-        header('Location: index.php');
-        exit();
-    }
-    require("connect.php");
-    $sql = mysql_query("SELECT `namn` FROM users WHERE id ='$id' limit 1");
-    $namn = mysql_result($sql,1);*/
-?>
 <!DOCTYPE html>
 <html lang="sv">
     <head>
@@ -24,6 +10,27 @@
     <body>
         <div id="storbox" class="indexpage">
             <header>
-                <h1>Gymnasiearbete</h1>            </header>            <div id="search">                <input type="text" placeholder="Sök">                <input type="submit" value="Sök">            </div>            <div id="sort">                <select>                    <option value="0">Alla</option>                    <option value="1">Sport</option>                    <option value="2">Mat</option>                    <option value="3">Film</option>                    <option value="4">Böcker</option>                </select>                <input type="submit" value="Sortera">            </div>            <div>                <input type="button" value="Lägg till" onclick="window.location.href='Add.php'">            </div>            <div id="main">            </div>        </div>
+                <h1>Gymnasiearbete</h1>
+            </header>
+            <div id="search">
+                <input type="text" placeholder="Sök">
+                <input type="submit" value="Sök">
+            </div>
+            <div id="sort">
+                <select>
+                    <option value="0">Alla</option>
+                    <option value="1">Sport</option>
+                    <option value="2">Mat</option>
+                    <option value="3">Film</option>
+                    <option value="4">Böcker</option>
+                </select>
+                <input type="submit" value="Sortera">
+            </div>
+            <div>
+                <input type="button" value="Lägg till" onclick="window.location.href='Add.html'">
+            </div>
+            <div id="main">
+            </div>
+        </div>
     </body>
 </html>
